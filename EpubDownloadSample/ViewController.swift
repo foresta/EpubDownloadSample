afef7edad23eeeba4d023efa5bb46e14a33c5b99
kz_morita <kz.morit@gmail.com>
2019-10-05 12:24:38 +0900

    Initial Commit

---
+++ EpubDownloadSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EpubDownloadSample
+//
+//  Created by kazuki.morita on 2019/10/05.
+//  Copyright © 2019 kz_morita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
